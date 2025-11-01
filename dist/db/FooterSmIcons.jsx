28586298f0140abb20e28150d4c3a76792106d91
Fozilbek Fayzullayev <fozilbekfayzullayev05@gmail.com>
2025-11-01 18:00:05 +0500

    first commit

---
+++ dist/db/FooterSmIcons.jsx
@@ -0,0 +1,35 @@
+import {
+  Facebook,
+  Mail,
+  Twitter,
+  WhatsApp,
+  YouTube,
+} from "@mui/icons-material";
+
+export const footerSocialMediaIcons = [
+  {
+    id: 1,
+    ariraLabel: "facebook",
+    icon: <Facebook />,
+  },
+  {
+    id: 2,
+    ariraLabel: "twitter",
+    icon: <Twitter />,
+  },
+  {
+    id: 3,
+    ariraLabel: "gmail",
+    icon: <Mail />,
+  },
+  {
+    id: 4,
+    ariraLabel: "youtube",
+    icon: <YouTube />,
+  },
+  {
+    id: 5,
+    ariraLabel: "what's up",
+    icon: <WhatsApp />,
+  },
+];
